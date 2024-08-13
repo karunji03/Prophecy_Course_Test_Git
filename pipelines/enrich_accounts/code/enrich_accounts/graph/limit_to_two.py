5a670e2ff61ae4a8780176bf94a012bc6647ee2e
kxb41840@ucmo.edu <kxb41840@ucmo.edu>
2024-08-13 20:01:46 +0000

    Enhances enrich_accounts pipeline with new transformations and filters

---
+++ pipelines/enrich_accounts/code/enrich_accounts/graph/limit_to_two.py
@@ -0,0 +1,10 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+from prophecy.libs import typed_lit
+from enrich_accounts.config.ConfigStore import *
+from enrich_accounts.functions import *
+
+def limit_to_two(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.limit(2)
--- pipelines/enrich_accounts/code/enrich_accounts/pipeline.py
+++ pipelines/enrich_accounts/code/enrich_accounts/pipeline.py
@@ -7,12 +7,19 @@ from prophecy.utils import *
 from enrich_accounts.graph import *
 
 def pipeline(spark: SparkSession) -> None:
+    df_salesforce_Account = salesforce_Account(spark)
+    df_filter_true = filter_true(spark, df_salesforce_Account)
+    df_rename_and_drop_columns = rename_and_drop_columns(spark, df_salesforce_Account)
+    df_employees_by_billing_city = employees_by_billing_city(spark, df_salesforce_Account)
     df_salesforce_Opportunity = salesforce_Opportunity(spark)
     df_selectfields_Opportunity = selectfields_Opportunity(spark, df_salesforce_Opportunity)
-    df_salesforce_Account = salesforce_Account(spark)
+    df_select_relevant_columns = select_relevant_columns(spark, df_salesforce_Account)
     df_aggregate_opportunities_by_account = aggregate_opportunities_by_account(spark, df_selectfields_Opportunity)
     df_account_join = account_join(spark, df_salesforce_Account, df_aggregate_opportunities_by_account)
     enriched_accounts(spark, df_account_join)
+    df_flatten_schema = flatten_schema(spark, df_salesforce_Account)
+    df_limit_to_two = limit_to_two(spark, df_salesforce_Account)
+    df_by_name_asc = by_name_asc(spark, df_salesforce_Account)
 
 def main():
     spark = SparkSession.builder\
